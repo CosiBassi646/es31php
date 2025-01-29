e81841b7d59c50af8b6a39600539d4b9fa8a826b
CosiBassi646 <bassicosimo@gmail.com>
2025-01-29 10:07:46 +0100

    l'output avviene in una tabella

--- action.php
+++ action.php
@@ -34,12 +34,29 @@
                 }
             }
         }
-        echo $nomeUtente;
-        echo "<br>";
-        echo $etaUtente;
-        echo "<br>";
-        echo $prezzoAbbonamento;
       ?>
+      <div class="w-50">
+          <table class="table text-center table-bordered">
+              <tr>
+                  <th colspan = "3">Dati inseriti nel form</th>
+                  <th colspan = "1">risposta del server</th>
+              </tr>
+              <tr>
+                  <th>Nome</th>
+                  <th>Età</th>
+                  <th>Pagamento</th>
+                  <th>Output</th>
+              </tr>
+                <tr>
+              <?php
+                echo"<td>$nomeUtente</td>";
+                echo"<td>$etaUtente</td>";
+                echo"<td>$tipoPagamento</td>";
+                echo"<td>$prezzoAbbonamento</td>";
+              ?>
+              </tr>
+          </table>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
